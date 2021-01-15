44554db1ec698c7c2fc4e005e20c7d2a44720632
Thor <the.thunder.god@gmail.com>
2021-01-15 13:02:54 -0500

    v0.1.0 Game Store and Registry Support
    -Can now detect installations via the EGS
    -The Registry is now the preferred method for detecting GoG
    -GoG installs now launch using the GoG launcher to enable achievements, friend notifications, and time tracking. Launch separately from Vortex if you do not want to go through the launcher.
    -Updated Imagery

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
-//Import some assets from Vortex we'll need.
 const path = require('path');
+const { fs, log, util } = require('vortex-api');
+const winapi = require('winapi-bindings');
 // Nexus Mods domain for the game. e.g. nexusmods.com/bloodstainedritualofthenight
 const GAME_ID = 'cyberpunk2077';
 //Steam Application ID, you can get this from https://steamdb.info/apps/
 const STEAMAPP_ID = '1091500';
 //GOG Application ID, you can get this from https://www.gogdb.org/
 const GOGAPP_ID = '1423049311';
-const { fs, log, util } = require('vortex-api');
+//Epic Application ID
+const EPICAPP_ID = 'Ginger';
 
 function main(context) {
-	//This is the main function Vortex will run when detecting the game extension. 
+  //This is the main function Vortex will run when detecting the game extension. 
 	context.registerGame({
         id: GAME_ID,
         name: 'Cyberpunk 2077',
         mergeMods: true,
         queryPath: findGame,
-        supportedTools: [],
         queryModPath: () => '',
         logo: 'gameart.jpg',
         executable: () => 'bin/x64/Cyberpunk2077.exe',
         requiredFiles: [
           'bin/x64/Cyberpunk2077.exe'
         ],
-        setup: undefined,
+        supportedTools: [],
+        requiresLauncher: requiresGoGLauncher,
+        setup: prepareForModding,
         environment: {
-          SteamAPPId: STEAMAPP_ID,
+          SteamAPPId: STEAMAPP_ID
         },
         details: {
           steamAppId: STEAMAPP_ID,
           gogAppId: GOGAPP_ID,
-        },
+          epicAppId: EPICAPP_ID
+        }
       });
 
     return true
 }
 
 function findGame() {
-    return util.GameStoreHelper.findByAppId([STEAMAPP_ID, GOGAPP_ID])
-        .then(game => game.gamePath);
+  try {
+    const instPath = winapi.RegGetValue(
+      'HKEY_LOCAL_MACHINE',
+      'SOFTWARE\\WOW6432Node\\GOG.com\\Games\\' + GOGAPP_ID,
+      'PATH');
+    if (!instPath) {
+      throw new Error('empty registry key');
+    }
+    console.log("Install Path: " + instPath.value)
+    return Promise.resolve(instPath.value);
+  } catch (err) {
+    return util.GameStoreHelper.findByAppId([STEAMAPP_ID,GOGAPP_ID,EPICAPP_ID])
+      .then(game => game.gamePath);
+  }    
+}
+
+function requiresGoGLauncher() {
+  return util.GameStoreHelper.isGameInstalled(GOGAPP_ID, 'gog')
+    .then(gog => gog
+      ? { launcher: 'gog', addInfo: GOGAPP_ID }
+      : undefined);
 }
 
 function prepareForModding(discovery) {
